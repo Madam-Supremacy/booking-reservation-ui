25e9eeaca42ba9cf8ea386552ceefce766ebedc8
Jabulile <jabulilemsibi184@gmail.com>
2026-02-04 10:31:09 +0200

    Delete target directory

--- target/classes/sql/insert_resources.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO resources (name, type, capacity, location) VALUES
-('Conference Room A', 'Room', 20, 'Building 1, Floor 3'),
-('Conference Room B', 'Room', 15, 'Building 1, Floor 3'),
-('Meeting Room 101', 'Room', 8, 'Building 2, Floor 1'),
-('Training Room', 'Room', 30, 'Building 2, Floor 2'),
-('Projector 1', 'Equipment', 1, 'Building 1, IT Department'),
-('Laptop 1', 'Equipment', 1, 'Building 2, IT Department'),
-('Auditorium', 'Venue', 100, 'Main Building'),
-('Cafeteria', 'Venue', 50, 'Ground Floor');
